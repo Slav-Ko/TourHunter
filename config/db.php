d3afddffc2843f7daec013e1203e8d380c2b358a
Slav-Ko <slavko.chita@gmail.com>
2017-06-25 14:06:49 +0300

    config admin email,mysql

--- config/db.php
+++ config/db.php
@@ -2,8 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=u0320006_tourhunter',
+    'username' => 'u0320006_root',
+    'password' => '22',
     'charset' => 'utf8',
+    'tablePrefix' => 'thai_',
 ];
